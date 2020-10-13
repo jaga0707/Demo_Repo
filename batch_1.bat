bbfe112a98f936224da93dffbed1f7c7f1787143
jaga <jajagandravid@gmail.com>
2020-10-13 12:32:36 +0530

    this is for tag3 commit

---
+++ batch_1.bat
@@ -0,0 +1,4 @@
+#!/bin/bash/
+echo " this is batch script for the tag 3 checking lets c it\n";
+echo " lets print the same \n";
+
---
+++ shell_1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash/bash
+# this is shell file for tag purpose
+echo " welcome to the shell \n"
+echo " this is for the tag "
